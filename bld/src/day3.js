4f4aec6e05ddc656f0d3359fc281f0cb9ac467f9
Parker Bossier <parkerbossier@gmail.com>
2022-01-06 02:05:19 -0500

    Day 4

---
+++ bld/src/day3.js
@@ -0,0 +1,69 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const day3data_1 = require("./day3data");
+function main1(data) {
+    const oneCounts = countOnes(data);
+    const gammaBinary = oneCounts
+        .map(c => {
+        return c > data.length - c ? '1' : '0';
+    })
+        .join('');
+    const epsilonBinary = oneCounts
+        .map(c => {
+        return c < data.length - c ? '1' : '0';
+    })
+        .join('');
+    const gamma = parseInt(gammaBinary, 2);
+    const epsilon = parseInt(epsilonBinary, 2);
+    console.log(gamma * epsilon);
+}
+function countOnes(data) {
+    const counts = new Array(data[0].length).fill(0);
+    data.forEach(row => {
+        for (let i = 0; i < row.length; i++) {
+            if (row[i] === '1')
+                counts[i]++;
+        }
+    });
+    return counts;
+}
+function main2(data) {
+    let o2Data = data;
+    for (let i = 0; i < data[0].length; i++) {
+        const oneCounts = countOnes(o2Data);
+        const keyOneCount = oneCounts[i];
+        const keyMode = (() => {
+            if (keyOneCount === o2Data.length - keyOneCount)
+                return '1';
+            else if (keyOneCount > o2Data.length - keyOneCount)
+                return '1';
+            else
+                return '0';
+        })();
+        o2Data = o2Data.filter(l => l[i] === keyMode);
+        if (o2Data.length === 1)
+            break;
+    }
+    let co2Data = data;
+    for (let i = 0; i < data[0].length; i++) {
+        const oneCounts = countOnes(co2Data);
+        const keyOneCount = oneCounts[i];
+        const keyMode = (() => {
+            if (keyOneCount === co2Data.length - keyOneCount)
+                return '0';
+            else if (keyOneCount > co2Data.length - keyOneCount)
+                return '0';
+            else
+                return '1';
+        })();
+        co2Data = co2Data.filter(l => l[i] === keyMode);
+        if (co2Data.length === 1)
+            break;
+    }
+    const o2 = parseInt(o2Data[0], 2);
+    const co2 = parseInt(co2Data[0], 2);
+    console.log(o2 * co2);
+}
+const data = day3data_1.day3data.split('\n');
+main1(data);
+main2(data);
