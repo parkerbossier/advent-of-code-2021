010d29ef8e4135900ad21cbf360faa9099b925da
Parker Bossier <parkerbossier@gmail.com>
2022-02-07 23:31:40 -0500

    Day 17

--- src/day17.ts
+++ src/day17.ts
@@ -2,15 +2,21 @@ import _ = require('lodash');
 
 type Point = [number, number];
 
-function simulate(initialVelocity: Point) {
+function simulate(initialVelocity: Point, log = false) {
 	const vel = [...initialVelocity];
 	const pos: Point = [0, 0];
 	const points: Point[] = [];
+
+	if (log)
+		printMap(points);
 
 	do {
 		pos[0] += vel[0];
 		pos[1] += vel[1];
 		points.push([...pos]);
+
+		if (log)
+			printMap(points);
 
 		if (vel[0] > 0)
 			vel[0]--;
@@ -35,13 +41,13 @@ function simulate(initialVelocity: Point) {
 		};
 }
 
-const targetXRange = [20, 30];
-// const targetXRange = [70, 96];
-const targetYRange = [-10, -5];
-// const targetYRange = [-179, -124];
+// const targetXRange = [20, 30];
+const targetXRange = [70, 96];
+// const targetYRange = [-10, -5];
+const targetYRange = [-179, -124];
 
 function pointHasPassedTarget(point: Point) {
-	return point[0] > targetXRange[1] || point[1] < targetYRange[1];
+	return point[0] > targetXRange[1] || point[1] < targetYRange[0];
 }
 
 function pointIsInTarget(point: Point) {
@@ -53,7 +59,7 @@ function pointIsInTarget(point: Point) {
 
 function printMap(points: Point[]) {
 	const xMax = Math.max(...points.map(p => p[0]), targetXRange[1]);
-	const yMax = Math.max(...points.map(p => p[1]));
+	const yMax = Math.max(...points.map(p => p[1]), 0);
 	const yMin = Math.min(...points.map(p => p[1]), targetYRange[0] - 1);
 	const map = _.range(yMax, yMin - 1, -1).map(y => {
 		const row = _.range(0, xMax + 1).map(x => {
@@ -69,13 +75,10 @@ function printMap(points: Point[]) {
 		return row;
 	});
 	console.log(map.map(r => r.join('')).join('\n'));
+	console.log();
 }
 
 function main1() {
-	// d = v + (v-1) + (v-2) + ... + 1 = 1 + ... + v == v * (v + 1) / 2
-	// https://www.wolframalpha.com/widgets/view.jsp?id=c778a2d8bf30ef1d3c2d6bc5696defad
-	const getVForD = (d: number) => (Math.sqrt(8 * d + 1) - 1) / 2;
-
 	const velXRange = [
 		Math.ceil(getVForD(targetXRange[0])),
 		Math.floor(getVForD(targetXRange[1]))
@@ -86,12 +89,31 @@ function main1() {
 	const result = simulate(initialVel);
 	const yMax = Math.max(...result.points.map(p => p[1]));
 	console.log(yMax);
-
-	// printMap(result.points);
 }
 
-function main2() {
+/**
+ * d = v + (v-1) + (v-2) + ... + 1 = 1 + ... + v == v * (v + 1) / 2
+ * https://www.wolframalpha.com/widgets/view.jsp?id=c778a2d8bf30ef1d3c2d6bc5696defad
+ */
+const getVForD = (d: number) => (Math.sqrt(8 * d + 1) - 1) / 2;
 
+function main2() {
+	const minVelX = Math.ceil(getVForD(targetXRange[0]));
+	const maxVelX = targetXRange[1];
+
+	const minVelY = targetYRange[0];
+	const maxVelY = -targetYRange[0] - 1;
+
+	const hits: [number, number][] = [];
+	for (let x = minVelX; x <= maxVelX; x++) {
+		for (let y = minVelY; y <= maxVelY; y++) {
+			const result = simulate([x, y]);
+			if (result.success)
+				hits.push([x, y]);
+		}
+	}
+
+	console.log(hits.length);
 }
 
 main1();
