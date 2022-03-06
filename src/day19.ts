885b33987fdbd4170a04ae269b7917437e2a4858
Parker Bossier <parkerbossier@gmail.com>
2022-03-06 00:38:24 -0500

    Day 19!

--- src/day19.ts
+++ src/day19.ts
@@ -2,49 +2,6 @@ import _ from 'lodash';
 import * as math from 'mathjs';
 import { day19data } from './day19data';
 
-function negateDiffString(diff: string) {
-	const parsed = JSON.parse(diff) as number[];
-	const negated = parsed.map(n => n * -1);
-	return JSON.stringify(negated);
-}
-
-function separationsMightMatch(a: number[], b: number[]) {
-	const aNorm = [...a].map(n => Math.abs(n)).sort();
-	const bNorm = [...b].map(n => Math.abs(n)).sort();
-	return _.isEqual(aNorm, bNorm);
-}
-
-
-/*
-p1 = [0, 1, 2]
-p2 = [2, 4, 1]
-sepA = [2, 3, -1]
-
-// rotate [0, -1, 2]
-p1 = [0, -1, 2]
-p2 = [2, -4, 1]
-sepB = [2, -3, -1]
-
-// rotate [0, -1, 2]
-sepAr = [2, -3, -1]
-
-
-initial
-x y z
-
-rotate around x
-x -z y
-
-rotate around y
-z y -x
-
-rotate around z
--y x z
-
-
-
-*/
-
 /** b1 - b0 */
 function subtractBeacons(b1: number[], b0: number[]) {
 	{
@@ -53,44 +10,83 @@ function subtractBeacons(b1: number[], b0: number[]) {
 }
 
 function main(scanners: Scanner[]) {
-	const s0 = scanners[0];
-	const s1 = scanners[1];
+	const [s0, ...unmatchedScanners] = scanners;
+	s0.getBeaconSeparations();
 
-	// console.log(s1.beacons[0])
-	// s1.translate([-36, 6, 33]);
-	// console.log(s1.beacons[1])
+	const matchedScanners = [s0];
 
-	const seps0 = s0.getBeaconSeparations().beaconSeparations2;
-	let seps1 = s1.getBeaconSeparations().beaconSeparations2;
+	while (unmatchedScanners.length > 0) {
+
+		const curScanner = unmatchedScanners.splice(0, 1)[0];
+		let foundMatch = false;
+		for (const matchedScanner of matchedScanners) {
+			const offset = tryMatchScanners(matchedScanner, curScanner);
+
+			if (offset) {
+				curScanner.translate(offset);
+				matchedScanners.push(curScanner);
+				foundMatch = true;
+				break;
+			}
+		}
+
+		if (!foundMatch) {
+			unmatchedScanners.push(curScanner);
+		}
+	}
+
+	console.log({ matchedScanners, unmatchedScanners });
+
+	const uniqueBeacons = _.uniqWith(matchedScanners.flatMap(s => s.beacons), _.isEqual);
+	console.log(uniqueBeacons.length);
+
+	let maxDistance = 0;
+	for (let i = 0; i < scanners.length; i++) {
+		for (let j = i + 1; j < scanners.length; j++) {
+			if (i === j) continue;
+
+			const distance = manhattanDistance(scanners[i].locationRelativeToS0, scanners[j].locationRelativeToS0);
+			if (distance > maxDistance)
+				maxDistance = distance;
+		}
+	}
+	console.log(maxDistance)
+}
+
+function manhattanDistance(s0: number[], s1: number[]) {
+	return s0.map((s0n, i) => Math.abs(s0n - s1[i])).reduce((a, b) => a + b, 0);
+}
+
+function tryMatchScanners(s0: Scanner, s1: Scanner) {
+	const s0Seps = s0.lastBeasonSeparations;
+
+	let offset: number[] | null = null;
 
 	breakout:
+	// facing x, y, z
 	for (let facingAxis = 0; facingAxis < 3; facingAxis++) {
+		// facing +, - `facingAxis`
 		for (let facingAxisDirection = 0; facingAxisDirection < 2; facingAxisDirection++) {
+			// rotate around facing
 			for (let rotation = 0; rotation < 4; rotation++) {
-				console.log({ facingAxis, facingAxisDirection, rotation });
-
 				s1.rotate(facingAxis, facingAxisDirection, rotation);
 
-				seps1 = s1.getBeaconSeparations().beaconSeparations2;
+				const s1Seps = s1.getBeaconSeparations();
 
-				const matchingPairs: number[][][] = [];
-				for (const s0 of seps0) {
-					const s1 = seps1.get(s0[0]);
+				const matchingSeps: number[][][] = [];
+				for (const s0 of s0Seps) {
+					const s1 = s1Seps.get(s0[0]);
 					if (s1) {
-						matchingPairs.push([
+						matchingSeps.push([
 							s0[1], s1
 						]);
 					}
 				}
 
-				console.log('matching pairs')
-				//console.log(matchingPairs);
-				console.log(matchingPairs.length);
-
-				if (matchingPairs.length < 12 * 6)
+				if (matchingSeps.length < 12 * 6)
 					continue;
 
-				const offsets = matchingPairs.flatMap(p => {
+				const offsets = matchingSeps.flatMap(p => {
 					const s0BeaconA = s0.beacons[p[0][0]];
 					const s0BeaconB = s0.beacons[p[0][1]];
 					const s1BeaconA = s1.beacons[p[1][0]];
@@ -110,27 +106,20 @@ function main(scanners: Scanner[]) {
 				if (bestOffset[1] < 12)
 					continue;
 
-				const offset: number[] = JSON.parse(bestOffset[0]);
-				console.log('offset', offset)
-
-				s1.translate(offset);
-
-				console.log('s0')
-				console.log(s0.beacons)
-				console.log('s1')
-				console.log(s1.beacons)
-
-				console.log('mutual')
-				console.log(_.intersectionWith(s0.beacons, s1.beacons, _.isEqual))
+				offset = JSON.parse(bestOffset[0]);
 
 				break breakout;
 			}
 		}
 	}
+
+	return offset;
 }
 
 class Scanner {
 	public beacons: number[][];
+	public lastBeasonSeparations: Map<string, number[]> = new Map();
+	public locationRelativeToS0 = [0, 0, 0];
 	public name: string;
 	private originalBeacons: number[][];
 
@@ -144,24 +133,35 @@ class Scanner {
 		this.originalBeacons = this.beacons;
 	}
 
+	/**
+	 * Get the separations between all beacons (and cache the result).
+	 */
 	getBeaconSeparations() {
-		const beaconSeparations: [number, number, number, number[]][] = [];
-		const beaconSeparations2 = new Map<string, number[]>();
+		const beaconSeparations = new Map<string, number[]>();
 
 		for (let i = 0; i < this.beacons.length; i++) {
 			for (let j = 0; j < this.beacons.length; j++) {
 				if (i === j) continue;
-				// for (let j = i + 1; j < this.beacons.length; j++) {
 				const iBeacon = this.beacons[i];
 				const jBeacon = this.beacons[j];
 				const diff = iBeacon.map((n, bi) => n - jBeacon[bi]);
-				beaconSeparations.push([parseInt(this.name, 10), i, j, diff]);
-				beaconSeparations2.set(JSON.stringify(diff), [i, j]);
+				beaconSeparations.set(JSON.stringify(diff), [i, j]);
 			}
 		}
-		return { beaconSeparations, beaconSeparations2 };
+		this.lastBeasonSeparations = beaconSeparations;
+		return beaconSeparations;
 	}
 
+	/**
+	 * (Preface: assume the original orientation of every scanner is facing [1, 0, 0] with the ceiling towards +z.)
+	 * 
+	 * First, rotate towards the `facing` axis (0-2 for xyz)
+	 * in the `facingDirection` direction (0 = negative, 1 = positive).
+	 * 
+	 * Second, rotate around the new facing vector by `rotation` quarter rotations.
+	 * 
+	 * Note: all rotation must happen before translation.
+	 */
 	rotate(facing: number, facingDirection: number, rotation: number) {
 		const facingVector = [
 			[1, 0, 0],
@@ -187,12 +187,16 @@ class Scanner {
 		this.beacons = newBeacons;
 	}
 
+	/**
+	 * Translates all beacons by `offset`.
+	 * Note: all transslation must happen after rotation.
+	 */
 	translate(offset: number[]) {
 		const newBeacons = this.beacons.map(b => {
 			return [b[0] + offset[0], b[1] + offset[1], b[2] + offset[2]];
 		});
-		this.originalBeacons = newBeacons;
 		this.beacons = newBeacons;
+		this.locationRelativeToS0 = offset;
 	}
 }
 
@@ -201,4 +205,3 @@ const data = day19data
 	.map(s => new Scanner(s));
 
 main(data);
-//main2();
